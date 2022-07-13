5bbdae9da98d4608863051bfe26f8e49ab63fad6
Beithirku <109206549+Beithirku@users.noreply.github.com>
2022-07-13 08:58:08 +0100

    Update 21 Code Test User Input.py

--- 21 Code Test User Input.py
+++ 21 Code Test User Input.py
@@ -1,3 +1,5 @@
+
+#Accepts 2 input values for A and X
 def multiplication(A, X):
     i = 1
     while i < 4:
@@ -10,8 +12,11 @@ def multiplication(A, X):
         print("\n")
         
         i += 1 
-
+#Asks the user for any integer which will be the starting times table.
 A = int(input("Enter an integer: "))
+
+#Asks the user for the end point of that table.
 X = int(input("Enter the end point for the table: "))
 
+#Calls the code.
 multiplication(A, X)
